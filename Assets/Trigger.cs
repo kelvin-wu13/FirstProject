224ad1516d79959b6ba07f9366a54f7ad4b9a3d9
kelvin-wu13 <kelvinwu04@gmail.com>
2024-02-07 12:35:35 +0700

    Project1
    -Add Bola
    -Add RigidBody
    -Add Audio

---
+++ Assets/Trigger.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trigger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource Iyahh;
+       private void OnTriggerEnter(Collider other)
+       {
+        print("GYATT");
+       } 
+}
